4cd72e8bf21650c89c8a0b445db3dff0c51812ec
Anirudh4030 <2311cs020017@mallareddyuniversity.ac.in>
2025-09-28 15:54:31 +0530

    Update Contact.jsx to use Render backend

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -20,7 +20,7 @@ export default function Contact() {
     setError('')
 
     try {
-      const res = await fetch('http://localhost:5000/send', {
+      const res = await fetch('https://golagraphic.onrender.com/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form)
